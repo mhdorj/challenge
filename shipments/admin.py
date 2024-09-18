0e02614c56b18a1c6f96be133fc58f7d5d48137a
mhd <mahdioruji8180@gmail.com>
2024-09-18 12:25:37 +0330

    Add admin panel

--- shipments/admin.py
+++ shipments/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Shipment, Article
 
-# Register your models here.
+
+@admin.register(Shipment)
+class ShipmentAdmin(admin.ModelAdmin):
+    list_display = ('tracking_number', 'carrier', 'sender_address', 'receiver_address', 'status')
+    search_fields = ('tracking_number', 'carrier', 'status')
+    list_filter = ('carrier', 'status')
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'price', 'sku', 'shipment')
+    search_fields = ('name', 'sku')
+    list_filter = ('shipment',)
